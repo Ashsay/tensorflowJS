4b51e39a4c28df8e8add10dbb300e656a4059272
ashsay <deemorhythm123@gamil.com>
2019-12-23 22:45:48 +0800

    tensor操作

--- tensor/script.js
+++ tensor/script.js
@@ -41,3 +41,19 @@ tf.transpose(tpArr).print();
 const cal = [1,2,3];
 tf.tensor(cal).print();
 tf.tensor(cal).dot(tf.tensor(tpArr)).print();
+
+// 张量切割操作
+const arrOfTensor2d = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const d2 = 2;
+//第一个为数组样本  [第一维度数量, 第二维度数量]
+const tensor2dArr = tf.tensor2d(arrOfTensor2d, [arrOfTensor2d.length/d2, d2]);
+tensor2dArr.print();
+//第一个位起点， 第二个位大小 [竖向数量，横向数量]
+const result2d = tensor2dArr.slice([2,0], [2,1]);
+result2d.print();
+
+//数据随机化  洗牌操作
+const arrOfShuffle = [1, 2, 3, 4, 5, 6, 7];
+//会改变原来的数组
+tf.util.shuffle(arrOfShuffle);
+console.log(arrOfShuffle);
